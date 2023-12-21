3c8b4db28116daee1a4e872795b1d0ff34575d70
Michal Przepiora <michal.przepiora@gmail.com>
2023-12-21 12:26:08 +0100

    Modify home view

--- app/src/main/java/pl/com/przepiora/shoppinglist/MainActivity.kt
+++ app/src/main/java/pl/com/przepiora/shoppinglist/MainActivity.kt
@@ -3,13 +3,22 @@ package pl.com.przepiora.shoppinglist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import pl.com.przepiora.shoppinglist.ui.theme.ShoppingListTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +26,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ShoppingListTheme {
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Box(
+                            modifier = Modifier.background(color = Color.Gray).fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "Shopping list:",
+                                fontFamily = FontFamily.SansSerif,
+                                fontSize = 27.sp
+                            )
+                            Text(
+                                modifier = Modifier.align(Alignment.CenterEnd),
+                                text = "Synch: 12.12.2023 12:00",
+                                textAlign = TextAlign.Right
+                            )
+                        }
+                    }
+
+
                 }
             }
         }
